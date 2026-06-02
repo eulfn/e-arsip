3e703dba34810d4db8db27a6c9b5bf0022c9e246
eulfen <necceds@gmail.com>
2026-06-02 16:26:11 +0800

    Delete ex directory

--- ex/dbtest.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "", "e_archive");
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-} else {
-    echo "Connected successfully!";
-}
